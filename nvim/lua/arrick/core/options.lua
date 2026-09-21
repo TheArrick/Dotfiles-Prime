360bd9bffbdabc842a7e60026f7fa81ace3522f3
Ignacio Jaldin <ignaciojaldin72@gmail.com>
2026-09-21 13:58:08 -0400

    Disable relative numbers

--- nvim/lua/arrick/core/options.lua
+++ nvim/lua/arrick/core/options.lua
@@ -2,7 +2,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-
 -- Disable netrw for nvim-tree compatibility
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -11,19 +10,17 @@ vim.g.loaded_netrwPlugin = 1
 -- deshabilitado en nvim-treesitter, causando crash en _get_injections.
 -- Forzamos detener treesitter al abrir markdown.
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "markdown" },
-    callback = function(ev)
-        pcall(vim.treesitter.stop, ev.buf)
-    end,
+	pattern = { "markdown" },
+	callback = function(ev)
+		pcall(vim.treesitter.stop, ev.buf)
+	end,
 })
-
 
 local opt = vim.opt
 local opts = { noremap = true, silent = true }
 
 -- UI & Editor options
 opt.number = true
-opt.relativenumber = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
@@ -56,48 +53,48 @@ vim.o.title = true
 -- Autocomandos para template C++
 local cpp_template_group = vim.api.nvim_create_augroup("CppTemplate", { clear = true })
 vim.api.nvim_create_autocmd("BufNewFile", {
-    group = cpp_template_group,
-    pattern = "*.cpp",
-    callback = function()
-        local template_path = vim.fn.expand("~/Workspace/Competitiva/Template/template.cpp")
-        if vim.fn.filereadable(template_path) == 1 then
-            vim.cmd("0r " .. template_path)
-        end
-    end,
+	group = cpp_template_group,
+	pattern = "*.cpp",
+	callback = function()
+		local template_path = vim.fn.expand("~/Workspace/Competitiva/Template/template.cpp")
+		if vim.fn.filereadable(template_path) == 1 then
+			vim.cmd("0r " .. template_path)
+		end
+	end,
 })
 
 -- Auto-renombrar ventana de tmux con el nombre del archivo (solo si se ejecuta dentro de tmux)
 if vim.env.TMUX and vim.fn.executable("tmux") == 1 then
-    local tmux_group = vim.api.nvim_create_augroup("TmuxAutoRename", { clear = true })
+	local tmux_group = vim.api.nvim_create_augroup("TmuxAutoRename", { clear = true })
 
-    vim.api.nvim_create_autocmd("BufEnter", {
-        group = tmux_group,
-        pattern = "*",
-        callback = function()
-            local filename = vim.fn.expand("%:t")
-            if filename ~= "" then
-                vim.fn.system("tmux rename-window " .. filename)
-            end
-        end,
-    })
+	vim.api.nvim_create_autocmd("BufEnter", {
+		group = tmux_group,
+		pattern = "*",
+		callback = function()
+			local filename = vim.fn.expand("%:t")
+			if filename ~= "" then
+				vim.fn.system("tmux rename-window " .. filename)
+			end
+		end,
+	})
 
-    vim.api.nvim_create_autocmd("BufLeave", {
-        group = tmux_group,
-        pattern = "*",
-        callback = function()
-            vim.fn.system("tmux rename-window nvim")
-        end,
-    })
+	vim.api.nvim_create_autocmd("BufLeave", {
+		group = tmux_group,
+		pattern = "*",
+		callback = function()
+			vim.fn.system("tmux rename-window nvim")
+		end,
+	})
 
-    vim.api.nvim_create_autocmd("VimEnter", {
-        group = tmux_group,
-        callback = function()
-            local filename = vim.fn.expand("%:t")
-            if filename ~= "" then
-                vim.fn.system("tmux rename-window " .. filename)
-            else
-                vim.fn.system("tmux rename-window nvim")
-            end
-        end,
-    })
+	vim.api.nvim_create_autocmd("VimEnter", {
+		group = tmux_group,
+		callback = function()
+			local filename = vim.fn.expand("%:t")
+			if filename ~= "" then
+				vim.fn.system("tmux rename-window " .. filename)
+			else
+				vim.fn.system("tmux rename-window nvim")
+			end
+		end,
+	})
 end
